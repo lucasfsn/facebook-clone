0ee4628e87cc903563c3fcecf2c5343f01e4c777
lukasz <luki424221@gmail.com>
2023-12-05 18:53:56 +0100

    Fixed path to reactions

--- client/data/reactions.ts
+++ client/data/reactions.ts
@@ -2,36 +2,36 @@ export const reactions = [
   {
     id: 1,
     name: "Like",
-    reaction: "../public/reaction-emoji/like.gif",
+    reaction: "/reaction-emoji/like.gif",
   },
   {
     id: 2,
     name: "Love",
-    reaction: "../public/reaction-emoji/love.gif",
+    reaction: "/reaction-emoji/love.gif",
   },
   {
     id: 3,
     name: "Care",
-    reaction: "../public/reaction-emoji/care.gif",
+    reaction: "/reaction-emoji/care.gif",
   },
   {
     id: 4,
     name: "Haha",
-    reaction: "../public/reaction-emoji/haha.gif",
+    reaction: "/reaction-emoji/haha.gif",
   },
   {
     id: 5,
     name: "Wow",
-    reaction: "../public/reaction-emoji/wow.gif",
+    reaction: "/reaction-emoji/wow.gif",
   },
   {
     id: 6,
     name: "Sad",
-    reaction: "../public/reaction-emoji/sad.gif",
+    reaction: "/reaction-emoji/sad.gif",
   },
   {
     id: 7,
     name: "Angry",
-    reaction: "../public/reaction-emoji/angry.gif",
+    reaction: "/reaction-emoji/angry.gif",
   },
 ];
