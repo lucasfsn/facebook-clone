467ddaf5321827ca35d9cdccce1899a96bdaf6d3
lukasz <luki424221@gmail.com>
2023-12-06 10:18:00 +0100

    Added image slider for user posts

--- client/src/features/post/ImagesGrid.tsx
+++ client/src/features/post/ImagesGrid.tsx
@@ -1,32 +1,47 @@
+import { useState } from "react";
+import ImageSlider from "../../ui/ImageSlider";
+
 interface ImagesGridProps {
   images: string[];
 }
 
 function ImagesGrid({ images }: ImagesGridProps) {
+  const [showSlider, setShowSlider] = useState(false);
+
+  function handleCloseSlider() {
+    setShowSlider(false);
+  }
+
   return (
-    <div className="grid grid-cols-6 gap-0.5 overflow-x-hidden">
-      {images.slice(0, 5).map((img, index) => (
-        <div
-          key={img}
-          className={`relative ${
-            images.length >= 5
-              ? index < 2
-                ? "col-span-3"
-                : "col-span-2"
-              : images.length % 2 !== 0 && index === 0
-              ? "col-span-6"
-              : "col-span-3"
-          }`}
-        >
-          <img src={img} className="h-full w-full object-cover" />
-          {index === 4 && images.length > 5 && (
-            <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 text-3xl font-semibold text-white">
-              +{images.length - 4}
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+    <>
+      {showSlider && <ImageSlider images={images} close={handleCloseSlider} />}
+      <div className="grid grid-cols-6 gap-0.5 overflow-x-hidden">
+        {images.slice(0, 5).map((img, index) => (
+          <div
+            key={img}
+            className={`relative ${
+              images.length >= 5
+                ? index < 2
+                  ? "col-span-3"
+                  : "col-span-2"
+                : images.length % 2 !== 0 && index === 0
+                ? "col-span-6"
+                : "col-span-3"
+            }`}
+          >
+            <img src={img} className="h-full w-full object-cover" />
+            {index === 4 && images.length > 5 && (
+              <div
+                className="absolute inset-0 flex cursor-pointer items-center justify-center bg-black bg-opacity-50 text-3xl font-semibold text-white hover:bg-opacity-[0.45]"
+                onClick={() => setShowSlider(true)}
+              >
+                +{images.length - 4}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
---
+++ client/src/ui/ImageSlider.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+
+interface ImageSliderProps {
+  images: string[];
+  close: () => void;
+}
+
+function ImageSlider({ images, close }: ImageSliderProps) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  const { ref } = useOutsideClick(close);
+
+  function handleShowPrev() {
+    setImageIndex((i) => {
+      if (i === 0) return images.length - 1;
+      return i - 1;
+    });
+  }
+
+  function handleShowNext() {
+    setImageIndex((i) => {
+      if (i === images.length - 1) return 0;
+      return i + 1;
+    });
+  }
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-50 backdrop-blur-sm">
+      <div
+        className="absolute left-1/2 top-1/2 z-50 h-1/3 w-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden shadow-2xl"
+        ref={ref}
+      >
+        <div className="flex h-full w-full flex-row overflow-hidden">
+          {images.map((image) => (
+            <div className="h-full w-full flex-shrink-0 flex-grow-0">
+              <img
+                key={image}
+                src={image}
+                className="slider-img h-full w-full object-cover"
+                style={{ translate: `${-100 * imageIndex}%` }}
+              />
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={handleShowPrev}
+          className="absolute bottom-0 left-0 top-0 w-[50px] bg-black bg-opacity-10 p-1 text-gray-700 transition-transform hover:-translate-x-[0.1rem] hover:text-gray-600"
+        >
+          <div className="mx-auto flex h-[35px] w-[35px] items-center justify-center rounded-full bg-white bg-opacity-30 p-1.5 shadow-md">
+            <MdKeyboardArrowLeft className="text-2xl" />
+          </div>
+        </button>
+        <button
+          onClick={handleShowNext}
+          className="absolute bottom-0 right-0 top-0 w-[50px] bg-black bg-opacity-10 p-1 text-gray-700 transition-transform hover:translate-x-[0.1rem] hover:text-gray-600"
+        >
+          <div className="mx-auto flex h-[35px] w-[35px] items-center justify-center rounded-full bg-white bg-opacity-30 p-1.5 shadow-md">
+            <MdKeyboardArrowRight className="text-2xl" />
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageSlider;
